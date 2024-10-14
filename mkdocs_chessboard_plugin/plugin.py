34c0821528a262ead980401859eefc5f58a52dfe
Oliver Schlüter <117340990+oscep@users.noreply.github.com>
2024-10-14 20:24:13 +0200

    set piecetheme and import jquery first

--- mkdocs_chessboard_plugin/plugin.py
+++ mkdocs_chessboard_plugin/plugin.py
@@ -11,10 +11,11 @@ class ChessboardPlugin(BasePlugin):
         def replace_fen(match):
             fen = match.group(1).strip()
             return f'''
-                    <div class="chessboard" id="board-{fen.replace(' ', '_')}"></div>
+                    <div class="chessboard" id="board-{fen.replace(' ', '_').replace('/', '_')}"></div>
                     <script>
-                        var board = Chessboard('board-{fen.replace(' ', '_')}', {{
-                            position: '{fen}'
+                        var board = Chessboard('board-{fen.replace(' ', '_').replace('/', '_')}', {{
+                            position: '{fen}',
+                            pieceTheme: 'https://chessboardjs.com/img/chesspieces/wikipedia/{'{piece}.png'}'
                         }});
                     </script>
                     '''
@@ -25,7 +26,9 @@ class ChessboardPlugin(BasePlugin):
         # Add the necessary CSS and JS for chessboard.js
         new_html = '''
                 <link rel="stylesheet" href="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css" integrity="sha384-q94+BZtLrkL1/ohfjR8c6L+A6qzNH9R2hBLwyoAfu3i/WCvQjzL2RQJ3uNHDISdU" crossorigin="anonymous">
+                <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
                 <script src="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js" integrity="sha384-8Vi8VHwn3vjQ9eUHUxex3JSN/NFqUg3QbPyX8kWyb93+8AC/pPWTzj+nHtbC5bxD" crossorigin="anonymous"></script>
+
                 ''' + new_html
 
         return new_html
