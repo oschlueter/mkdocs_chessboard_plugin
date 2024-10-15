00652e64bbc101d61166b3293e6568993973162f
Oliver Schlüter <117340990+oscep@users.noreply.github.com>
2024-10-15 06:29:37 +0200

    work in progress

--- mkdocs_chessboard_plugin/plugin.py
+++ mkdocs_chessboard_plugin/plugin.py
@@ -4,30 +4,48 @@ from mkdocs.plugins import BasePlugin
 
 
 class ChessboardPlugin(BasePlugin):
-    @staticmethod
-    def lichess_form(fen: str, stockfish_level: int = 8):
-        player = 'white' if fen.split(' ')[1] == 'w' else 'black'
 
-        return f'''
-        <form id="form-{fen.replace(' ', '_').replace('/', '_')}" action="https://lichess.org/setup/ai" method="POST" style="display: none;">
-                        <input type="hidden" name="fen" value="{fen}">
-                        <input type="hidden" name="variant" value="3">
-                        <input type="hidden" name="timeMode" value="1">
-                        <input type="hidden" name="time" value="3">
-                        <input type="hidden" name="time_range" value="7">
-                        <input type="hidden" name="increment" value="0">
-                        <input type="hidden" name="increment_range" value="0">
-                        <input type="hidden" name="days" value="2">
-                        <input type="hidden" name="days_range" value="2">
-                        <input type="hidden" name="mode" value="0">
-                        <input type="hidden" name="ratingRange" value="1305-2305">
-                        <input type="hidden" name="ratingRange_range_min" value="-500">
-                        <input type="hidden" name="ratingRange_range_max" value="500">
-                        <input type="hidden" name="level" value="{stockfish_level}">
-                        <input type="hidden" name="color" value="{player}">
-                    </form>
-                    <a href="#" class="post-link" onclick="document.getElementById('form-{fen.replace(' ', '_').replace('/', '_')}').submit(); return false;">Play against Stockfish level {stockfish_level}</a>
+    def send_post_requests(self):
+        return '''
+        
+<script>
+    function sendPostRequest(level, clockLimit, clockIncrement, days, color, variant, fen) {
+        const url = 'https://lichess.org/api/challenge/ai';
+        const data = {
+            level: level,
+            clock: {
+                limit: clockLimit,
+                increment: clockIncrement
+            },
+            days: days,
+            color: color,
+            variant: variant,
+            fen: fen
+        };
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            const challengeUrl = `https://lichess.org/${data.id}`;
+            navigator.clipboard.writeText(challengeUrl).then(() => {
+                console.log('Challenge URL copied to clipboard:', challengeUrl);
+            }).catch(err => {
+                console.error('Failed to copy URL to clipboard:', err);
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+</script>
         '''
+
 
     def on_page_content(self, html, **kwargs):
         # Regular expression to find code blocks starting with ```FEN
@@ -46,16 +64,18 @@ class ChessboardPlugin(BasePlugin):
                             pieceTheme: 'https://chessboardjs.com/img/chesspieces/wikipedia/{'{piece}.png'}'
                         }});
                     </script>
-                    {self.lichess_form(fen, 7)}
+                    
+                    <a href="#" class="post-link" onclick="sendPostRequest('{fen}', 7, '{player}'); return false;">Play against Stockfish level 7.</a>
                     <br>
-                    {self.lichess_form(fen, 8)}
+                    <a href="#" class="post-link" onclick="sendPostRequest('{fen}', 8, '{player}'); return false;">Play against Stockfish level 8.</a>
                     '''
 
         # Replace all FEN code blocks in the HTML
         new_html = fen_pattern.sub(replace_fen, html)
 
         # Add the necessary CSS and JS for chessboard.js
-        new_html = '''
+        new_html = f'''
+                {self.send_post_requests()} 
                 <link rel="stylesheet" href="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css" integrity="sha384-q94+BZtLrkL1/ohfjR8c6L+A6qzNH9R2hBLwyoAfu3i/WCvQjzL2RQJ3uNHDISdU" crossorigin="anonymous">
                 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
                 <script src="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js" integrity="sha384-8Vi8VHwn3vjQ9eUHUxex3JSN/NFqUg3QbPyX8kWyb93+8AC/pPWTzj+nHtbC5bxD" crossorigin="anonymous"></script>
